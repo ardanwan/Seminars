c9ccbfddb914ca8502365a3c6a17c50593a789f2
charlottefrenzen <charlottefrenzen@gmail.com>
2020-09-24 17:11:27 +0200

    modification of functions

--- assignment_1.py
+++ assignment_1.py
@@ -15,9 +15,7 @@ def lower_case(string):
 
     """
 
-    ### your code starts here
-
-    ### your code ends here
+    lower_string = string.lower()
 
     return lower_string
 
@@ -34,11 +32,9 @@ def upper_case(string):
 
     Returns:
     upper_string -- upper case version of string
-    
+
     """
 
-    ### your code starts here
+    upper_string = string.upper()
 
-    ### your code ends here
-
-    return lower_string
+    return upper_string
