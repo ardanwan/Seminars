66fd74af169ab2e8f6d2a1bc56596d3827d3266a
karelgeraedts <kkm.geraedts@gmail.com>
2020-09-24 15:42:49 +0200

    Finished assignment 1a and doc for 2b

--- assignment_2b.py
+++ assignment_2b.py
@@ -5,6 +5,14 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2b(string_1, string_2):
+    """
+    Argument:
+    string_1, string_2 - text you want to combine
+
+    Return:
+    a dict with L: string_1 in lower letters, R: string_2 in capital letters
+    and C: string 1 and 2 combined
+    """
 
     lower = string_1.lower()
     upper = string_2.upper()
