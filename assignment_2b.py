c758320d84472a1e58eaa9e199cf93637e8cdc4b
youri <yourimoll1994@gmail.com>
2020-09-24 15:50:04 +0200

    2b and c

--- assignment_2b.py
+++ assignment_2b.py
@@ -5,7 +5,13 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2b(string_1, string_2):
+    """
+    Returns a dict conaining the first parameter, converted to lower case, the
+    second parameter to upper and the two input string appended after one another.
 
+    @param String first input string, will be converted to lower case
+    @param String second input string, will be converted to upper case
+    """
     lower = string_1.lower()
     upper = string_2.upper()
     combined = string_1 + string_2
--- assignment_2c.py
+++ assignment_2c.py
@@ -5,6 +5,21 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2c(w, x, y, z):
+    """
+    Function that takes four numbers, as input.
+
+    returns a dictionary with the following key-value pairs:
+
+    multiply:   x * y
+    divide:     x / y
+    add:        w + z
+    subtract    w - z
+
+    @param w first input
+    @param x second input
+    @param y third input
+    @param z fourth input
+    """
 
     multiplication = x * y
     division = x / y
