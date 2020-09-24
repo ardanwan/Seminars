de0d6d284d46053c0ee58cf2d8b0985bc9aac14a
kamielgulpen <k.gulpen@hotmail.nl>
2020-09-24 15:55:28 +0200

    added documentation

--- assignment_2b.py
+++ assignment_2b.py
@@ -5,7 +5,13 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2b(string_1, string_2):
-
+    """
+    Takes as input 2 strings and gives as output a dictionary. The dictionary with
+    key "L" gives back the first string in lowercase, the dictionary with key "U"
+    gives back the second string in uppercase and the dictionary with key "C" 
+    gives a third output which is a combination of both strings
+    of both strings
+    """
     lower = string_1.lower()
     upper = string_2.upper()
     combined = string_1 + string_2
