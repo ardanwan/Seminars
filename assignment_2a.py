51f2bb1d14d9d24843ffb283e3eef403d0d18956
Ram0nB <45173421+Ram0nB@users.noreply.github.com>
2020-09-24 18:58:12 +0200

    assignment 2a done
    added the values for function_2b and function_2c so that this function prints the following:
    "Good job!
    Well done!
    Excellent! "

--- assignment_2a.py
+++ assignment_2a.py
@@ -9,11 +9,11 @@ Run the script to see if you succeeded! PS: Multiple combinations are possible,
 just give a correct one.
 """
 
-var_1 = function_2b(...)
+var_1 = function_2c(1000, 1000, 1000, 50)['subtract']
 
-var_2 = function_2c(...)
+var_2 = function_2b('Seminars', 'Borrel')['C']
 
-var_3 = str(function_2b(...)) + function_2c(...)
+var_3 = str(function_2c(10, 10, 1000, 1000)['multiply']) + function_2b('CLS', 'CLS')['L']
 
 if var_1 == 950:
     print("Good job!")
