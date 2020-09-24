07daf8d01dd6463ebf83db86e6595581e1de47e6
Loek21 <ljvansteijn@outlook.com>
2020-09-24 15:59:24 +0200

    2b

--- assignment_2b.py
+++ assignment_2b.py
@@ -5,7 +5,10 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2b(string_1, string_2):
-
+    """
+    Function takes two strings, transforms string1 to lowercase, string2 to uppercase. It uses both strings to create a combined string
+    string1 + string2. Returns dictionary containing three keys one for each type of string.
+    """
     lower = string_1.lower()
     upper = string_2.upper()
     combined = string_1 + string_2
