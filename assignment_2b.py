94f0ac42ba0f0fab3b8029ce7ddf322c6e6acf99
Roboraw <s.poelmann@xs4all.nl>
2020-09-24 15:36:36 +0200

    Update assignment_2b.py

--- assignment_2b.py
+++ assignment_2b.py
@@ -5,7 +5,7 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2b(string_1, string_2):
-
+    """ Returns a dict of string 1 (lower case), string 2 (uppercase) and combined (lower 1 and upper 2) keys = LUC"""
     lower = string_1.lower()
     upper = string_2.upper()
     combined = string_1 + string_2
