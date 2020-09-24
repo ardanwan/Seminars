483f6ba0e3de7cbd2ca4b63c15aaf3ae4f877c63
ChrisCleaner <christoph.kruger@yahoo.com>
2020-09-24 16:09:48 +0200

    With Documentation

--- assignment_2b.py
+++ assignment_2b.py
@@ -5,6 +5,14 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2b(string_1, string_2):
+    
+    """
+    Takes in two strings (string_1, string_2.
+    Returns dict with:
+        dict[0] = string_1 as lower case
+        dict[1] = string 2 as upper case
+        dict[2] = string_1 as lower case + string_2 as higher case
+    """
 
     lower = string_1.lower()
     upper = string_2.upper()
