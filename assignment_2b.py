45d08df46f27dd00f683e68b1b4bb2a4e67939ab
goodDayWithChicken <ignas.krikstaponis@outlook.com>
2020-09-24 15:53:06 +0200

    Update assignment_2b.py

--- assignment_2b.py
+++ assignment_2b.py
@@ -5,6 +5,21 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2b(string_1, string_2):
+    '''
+    
+
+    Parameters
+    ----------
+    string_1 : text you want to turn into lower case.
+    string_2 : text you want to turn into lower case
+
+    Returns
+    -------
+    returns dict {L: string_1 in lower case,
+                       U: string_2 in upper case,
+                       C: string_1 and string_2 combined into one string}.
+
+    '''
 
     lower = string_1.lower()
     upper = string_2.upper()
@@ -15,3 +30,6 @@ def function_2b(string_1, string_2):
                "C": combined}
 
     return dict
+
+
+print(function_2b("ignaS", "Bignas"))
