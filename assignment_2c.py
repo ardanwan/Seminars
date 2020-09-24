ec921f46749d2a9d42682127c3849feffb0e63d3
GLaDAP <GLaDAP@users.noreply.github.com>
2020-09-24 16:20:11 +0200

    Added documentation for function_2c

--- assignment_2c.py
+++ assignment_2c.py
@@ -5,7 +5,20 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2c(w, x, y, z):
+    """
+    Argument:
+    - w : first integer
+    - x : second integer
+    - y : third integer
+    - z : fourth integer
 
+    Returns:
+    results -- A dictionary with the following keys:
+    - 'multiply': containing the product of x * y
+    - 'divide'  : the division of x / y
+    - 'add'     : the result of w + z
+    - 'subtract': the result of w - z
+    """
     multiplication = x * y
     division = x / y
     addition = w + z
