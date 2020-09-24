81f18fb4baf43d0a3d2d7fefd185ec34bd661575
OscardeKeijzer <71188715+OscardeKeijzer@users.noreply.github.com>
2020-09-24 17:19:08 +0200

    Update assignment_2c.py

--- assignment_2c.py
+++ assignment_2c.py
@@ -1,8 +1,9 @@
 """
-Write a documentation for the simple function below. Your partner will have to
-implement the function, without knowing the code. Send your partner the
-documentation and see if he can work with it.
-No cheating! Don't show or tell hem the code directly
+Function 2c takes four numbers as arguments and performs four operations for which it creates new corresponding variables: multiplication between the 2nd and 3rd arguments, division of the 2nd arg. by the 3rd, addition of the 1st and 4th args., and subtraction of the 1st arg. from the 4th.
+
+The function then creates a dictionary named "results" that lists the four operations followed by their values.
+
+The function then returns results.
 """
 def function_2c(w, x, y, z):
 
