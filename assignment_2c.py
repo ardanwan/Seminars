4468b2f59cd80dd607f202aaee6dcc23996f8824
louisepos <35340560+louisepos@users.noreply.github.com>
2020-09-24 17:59:32 +0200

    Added documentation to 2b and 2c and removed code

--- assignment_2c.py
+++ assignment_2c.py
@@ -11,9 +11,25 @@ def function_2c(w, x, y, z):
     addition = w + z
     subtraction = w - z
 
-    results = {"multiply": multiplication,
-               "divide": division,
-               "add": addition,
-               "subtract": subtraction}
+"""
+Documentation function_2c:
+	Input:
+		We take as input four arguments w, x, y. z.
+
+	Code/Output:
+		We create a dictionary which will contain four different keys with each its own corresponding value.
+		https://www.w3schools.com/python/python_dictionaries.asp
+
+		key 1: "multiply", key 2: "divide", key 3: "add", key 4: "subtract"
+
+		The values are the variables which we created in the code:
+		value 1: x*y
+		value 2: x/y
+		value 3: w+z
+		value 4: w-z
+
+		This dictionary is returned as the output.
+"""
+
 
     return results
