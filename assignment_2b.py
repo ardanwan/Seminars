064231f39eff585295c0b0f1e030cd72a6c5b681
Lars-Water <larryvanderwater@hotmail.com>
2020-09-24 16:14:55 +0200

    assignment 2b

--- assignment_2b.py
+++ assignment_2b.py
@@ -4,6 +4,26 @@ implement the function, without knowing the code. Send your partner the
 documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
+
+""" 
+Function:
+
+function_2b = requires two strings as input
+
+Argument:
+
+lower = makes the first string into lowercase
+upper = makes the second string into lowercase
+combined = adds the two strings together
+
+dict = Library with keys L, U and C which stand for the arguments of this function
+
+Returns:
+
+dict
+
+"""
+
 def function_2b(string_1, string_2):
 
     lower = string_1.lower()
