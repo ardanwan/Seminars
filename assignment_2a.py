37d4b62470f27f3974d6603dca6275809060a06e
Jop Keuning <jop.keuning@xs4all.nl>
2020-09-24 15:56:06 +0200

    docu

--- assignment_2a.py
+++ assignment_2a.py
@@ -9,6 +9,28 @@ Run the script to see if you succeeded! PS: Multiple combinations are possible,
 just give a correct one.
 """
 
+'''
+function_2b(string1, string2):
+	Takes in 2 strings as arguments.
+
+	Returns a dict with 3 strings as values. 
+		Transforms string1 into lowercase strings.
+		Transforms string2 into uppercase strings
+		Combines String 1 and 2 together.
+		Puts them in a dict with keys "L" for lower, "U" for upper and "C" for combined
+
+
+function_2c(w, x, y, z):
+	takes in 4 integer arguments, w, x, y, z.
+	Returns a dict with 4 integers as values.:
+		the keyse are: "multiply", mulltiplies x with y.
+		"divide", divides x by y.
+		"add", Add z to w.
+		"subtract", Subtracts z from w.
+
+'''
+
+
 var_1 = function_2b(...)
 
 var_2 = function_2c(...)
