70a1ed4b714f983c7d4d9a229a7e5584c760c6e9
WesleyOMorrow <39685540+WesleyOMorrow@users.noreply.github.com>
2020-09-24 16:02:15 +0200

    Add files via upload

--- assignment_1.py
+++ assignment_1.py
@@ -9,10 +9,8 @@ def lower_case(string):
     """
     Argument:
     string -- text you want to turn into lower case
-
     Returns:
     lower_string -- lower case version of string
-
     """
 
     ### your code starts here
@@ -31,14 +29,13 @@ def upper_case(string):
     """
     Argument:
     string -- text you want to turn into upper case
-
     Returns:
     upper_string -- upper case version of string
     
     """
 
     ### your code starts here
-
+    upper_string = string.upper()
     ### your code ends here
 
-    return lower_string
+    return upper_string
