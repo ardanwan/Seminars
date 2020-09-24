6a3a5894ec2876898c16071b981d4e5cc29422d3
Jop Keuning <jop.keuning@xs4all.nl>
2020-09-24 15:42:02 +0200

    Sorry Daniel

--- assignment_1.py
+++ assignment_1.py
@@ -14,10 +14,7 @@ def lower_case(string):
     lower_string -- lower case version of string
 
     """
-
-    ### your code starts here
-
-    ### your code ends here
+    lower_string = string.lower()
 
     return lower_string
 
@@ -37,8 +34,5 @@ def upper_case(string):
     
     """
 
-    ### your code starts here
-
-    ### your code ends here
-
+    lower_string = string.upper()
     return lower_string
