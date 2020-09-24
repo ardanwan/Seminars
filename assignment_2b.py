8d57f5dffe64b191136b219ee6970458e425b1d4
Joos Akkerman <akkermanjoos@gmail.com>
2020-09-24 17:09:14 +0200

    Foutje hersteld

--- assignment_2b.py
+++ assignment_2b.py
@@ -4,14 +4,14 @@ implement the function, without knowing the code. Send your partner the
 documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
-
 def function_2b(string_1, string_2):
 
-    # create two strings, lower and upper. lower is string 1 made all lower case,
-    # upper is string 2 made upper case.
+    lower = string_1.lower()
+    upper = string_2.upper()
+    combined = string_1 + string_2
 
-    # combine lower and upper
+    dict = {"L": lower,
+               "U": upper,
+               "C": combined}
 
-    # return a dict with L=lower, U=upper and C=combined
-
-    pass
+    return dict
