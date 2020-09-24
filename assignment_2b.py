b9933aa547e2217e4d928ce67ff506050ed05655
Joos Akkerman <akkermanjoos@gmail.com>
2020-09-24 15:56:11 +0200

    Succes Guido

--- assignment_2b.py
+++ assignment_2b.py
@@ -4,14 +4,14 @@ implement the function, without knowing the code. Send your partner the
 documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
+
 def function_2b(string_1, string_2):
 
-    lower = string_1.lower()
-    upper = string_2.upper()
-    combined = string_1 + string_2
+    # create two strings, lower and upper. lower is string 1 made all lower case,
+    # upper is string 2 made upper case.
 
-    dict = {"L": lower,
-               "U": upper,
-               "C": combined}
+    # combine lower and upper
 
-    return dict
+    # return a dict with L=lower, U=upper and C=combined
+
+    pass
