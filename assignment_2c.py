125ea0fb287a2759094ecd4d54a05e989b1dafb6
YunxiangLi <56443059+YunxiangLi@users.noreply.github.com>
2020-09-25 00:26:28 +0800

    Update assignment_2c.py

--- assignment_2c.py
+++ assignment_2c.py
@@ -5,7 +5,17 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2c(w, x, y, z):
+    """return a dict with multiplication, division, addition, subtraction by given 4 inputs
 
+    :param w: parameter that add z or minus z
+    :param x: parameter that multiply y or divided by y
+    :param y: parameter that multiply x or divide x
+    :param z: parameter that is added to w or subtrated by w
+    :return: a dict with four pair key and value: multiplication, x * y; division, x / y; addition, w + z; subtraction, w - z
+
+    >>> function_2c(1,2,4,8)
+    {'multiply': 8, 'divide': 0.5, 'add': 9, 'subtract': -7}
+    """
     multiplication = x * y
     division = x / y
     addition = w + z
@@ -17,3 +27,6 @@ def function_2c(w, x, y, z):
                "subtract": subtraction}
 
     return results
+
+dict1 = {'multiply': 8, 'divide': 0.5, 'add': 9, 'subtract': -7}
+print(dict1['add'])
