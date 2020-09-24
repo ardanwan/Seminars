2a8e0df8a4f81e638e833aebf90d402c631f74d1
mbruin <menno.bruin@student.uva.nl>
2020-09-24 15:49:43 +0200

    added documention

--- assignment_2b.py
+++ assignment_2b.py
@@ -4,14 +4,27 @@ implement the function, without knowing the code. Send your partner the
 documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
+
+
 def function_2b(string_1, string_2):
+
+    """
+
+    Args:
+        string_1: String converted to lowercase
+        string_2: String converted to uppercase
+
+    Returns:
+        Dict containing lowercase string_1, uppercase string_2, and combination of string_1 and string_2
+
+    """
 
     lower = string_1.lower()
     upper = string_2.upper()
     combined = string_1 + string_2
 
     dict = {"L": lower,
-               "U": upper,
-               "C": combined}
+            "U": upper,
+            "C": combined}
 
     return dict
