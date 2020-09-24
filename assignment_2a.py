b398bbff848b21c62031882bd3f6e1c60c7e4930
ccfelius <lottefelius@Hotmail.com>
2020-09-24 16:05:30 +0200

    func 2a working

--- assignment_2a.py
+++ assignment_2a.py
@@ -1,5 +1,9 @@
 from assignment_2b import function_2b
 from assignment_2c import function_2c
+
+print(function_2b.__doc__)
+print(function_2c.__doc__)
+
 """
 Used the imported functions (function_2b and function_2c) and the documentation
 provided by your teammates to generate the correct answers below. You can use
@@ -9,11 +13,18 @@ Run the script to see if you succeeded! PS: Multiple combinations are possible,
 just give a correct one.
 """
 
-var_1 = function_2b(...)
+string1 = "Seminars"
+string2 = "CLS"
+w = 5
+x = 10
+y = 1000
+z = -50
 
-var_2 = function_2c(...)
+var_1 = function_2b(string1, string2)
 
-var_3 = str(function_2b(...)) + function_2c(...)
+var_2 = function_2c(w, x, y, z)
+
+var_3 = str(function_2b(string1, string2)) + str(function_2c(w,x,y,z))
 
 if var_1 == 950:
     print("Good job!")
