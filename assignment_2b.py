5e2afd04401a5baf8d8d37399aa524f0acb9fdf1
Viviane Desgrange <viviane.desgrange@gmail.com>
2020-09-24 15:55:56 +0200

    Add some documentation to assignment_2b and assignment_2c.

--- assignment_2b.py
+++ assignment_2b.py
@@ -5,7 +5,13 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2b(string_1, string_2):
-
+    """
+    Get lower case from string_1, upper case from string_2, then combined them.
+    Return a dictionary with lower_case of string_1, upper_case of string_2 and combined of both of them.
+    :param string_1: string_1 will be change to lower case
+    :param string_2: string_2 will be change to upper case
+    :return: dictionary
+    """
     lower = string_1.lower()
     upper = string_2.upper()
     combined = string_1 + string_2
--- assignment_2c.py
+++ assignment_2c.py
@@ -5,7 +5,17 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2c(w, x, y, z):
-
+    """
+    Multiply x with y
+    Divide x with y
+    Add w with z
+    Substract z to w
+    :param w: integer
+    :param x: integer
+    :param y: integer
+    :param z: integer
+    :return: dictionnary with the results of the 4 operation, named respectively: multiply, divide, add, substract.
+    """
     multiplication = x * y
     division = x / y
     addition = w + z
