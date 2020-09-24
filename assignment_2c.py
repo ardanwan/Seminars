d0edd9879ecb92c51d87a7d6968685bd0f44cad2
Peer96 <43616897+Peer96@users.noreply.github.com>
2020-09-24 16:46:42 +0200

    Add files via upload

--- assignment_2c.py
+++ assignment_2c.py
@@ -4,16 +4,12 @@ implement the function, without knowing the code. Send your partner the
 documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
+
+
 def function_2c(w, x, y, z):
+    ""
+   funtion returns a dictionary named results, keys are multiply,divide,add,subtract
 
-    multiplication = x * y
-    division = x / y
-    addition = w + z
-    subtraction = w - z
-
-    results = {"multiply": multiplication,
-               "divide": division,
-               "add": addition,
-               "subtract": subtraction}
-
-    return results
+ w and z have to be added, z subtracted from w
+ x and y multiplied, x divided by w
+    ""
