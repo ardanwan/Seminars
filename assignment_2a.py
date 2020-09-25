9dc26a74d3467ff682a388e3dd118a3c7a9f61f6
HaitzeHaitze <Haitze-devries@hotmail.com>
2020-09-25 10:52:27 +0200

    Add files via upload

--- assignment_2a.py
+++ assignment_2a.py
@@ -9,11 +9,11 @@ Run the script to see if you succeeded! PS: Multiple combinations are possible,
 just give a correct one.
 """
 
-var_1 = function_2b(...)
+var_1 = ((function_2c(1000,5,10,-50)["add"]))
 
-var_2 = function_2c(...)
+var_2 = ((function_2b("Seminars","Borrel")["C"]))
 
-var_3 = str(function_2b(...)) + function_2c(...)
+var_3 = str((function_2c(5,10,100,1000)["multiply"])) + function_2b(("CLS","cLs")["L"])
 
 if var_1 == 950:
     print("Good job!")
