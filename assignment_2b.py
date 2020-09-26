36269ab0214834a7a248bebedf4c6e0de65a00fa
WesleyOMorrow <39685540+WesleyOMorrow@users.noreply.github.com>
2020-09-26 16:36:43 +0200

    Add a documentation

--- assignment_2b.py
+++ assignment_2b.py
@@ -5,7 +5,15 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2b(string_1, string_2):
+	'''
+    Input: Two strings str1, str2
+    Return: A dict with following results:
 
+    L: str1 as lowercase
+    U: str2 as uppercase
+    C: combine str1 + str2
+	'''
+    
     lower = string_1.lower()
     upper = string_2.upper()
     combined = string_1 + string_2
