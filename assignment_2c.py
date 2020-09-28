d9a087326bff81809099644e17b6d9a31c278404
ChrisCleaner <61554538+ChrisCleaner@users.noreply.github.com>
2020-09-28 11:28:57 +0200

    Update assignment_2c.py

--- assignment_2c.py
+++ assignment_2c.py
@@ -8,10 +8,10 @@ def function_2c(w, x, y, z):
     """
     Takes in four variables (v1, v2, v3, v4). 
     returns dict with:
-        multiply: v2 * v3
-        divide: v2 / v3
-        add: v1 + v4
-        subtract: v1 - v4
+        "multiply": v2 * v3
+        "divide": v2 / v3
+        "add": v1 + v4
+        "subtract": v1 - v4
     """
 
     multiplication = x * y
