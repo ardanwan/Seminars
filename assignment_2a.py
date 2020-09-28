68c244f79ca5f058b584f5a2c95704420b7f0071
Susy Maijer <susy.maijer@student.uva.nl>
2020-09-28 17:35:01 +0200

    Forgotten fix for var 3

--- assignment_2a.py
+++ assignment_2a.py
@@ -15,7 +15,7 @@ var_1 = function_2c(...)
 
 var_2 = function_2b(...)
 
-var_3 = str(function_2b(...)) + function_2c(...)
+var_3 = str(function_2c(...)) + function_2d(...)
 
 if var_1 == 950:
     print("Good job!")
