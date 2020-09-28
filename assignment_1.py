af7a799ada9e8b629561a9946272a35a2214f8c7
S Kuilboer <s.kuilboer@gmail.com>
2020-09-28 10:40:42 +0200

    make a merge conflict

--- assignment_1.py
+++ assignment_1.py
@@ -17,7 +17,7 @@ def lower_case(string):
 
     ### your code starts here
 
-    lower_string = string.lower()
+    lower_string = string.trylower()
 
     ### your code ends here
 
@@ -41,7 +41,7 @@ def upper_case(string):
 
     ### your code starts here
 
-    upper_string = string.upper()
+    upper_string = string.tryupper()
 
     ### your code ends here
 
