c68d5edcbb12ca353abc223e90dc493061660d77
RyanMcDonnell26 <49677447+RyanMcDonnell26@users.noreply.github.com>
2020-10-01 17:38:34 +0200

    all changes

--- assignment_2a.py
+++ assignment_2a.py
@@ -9,11 +9,11 @@ Run the script to see if you succeeded! PS: Multiple combinations are possible,
 just give a correct one.
 """
 
-var_1 = function_2b(...)
+var_1 = function_2c(1000, 1, 1, -50)['add']
 
-var_2 = function_2c(...)
+var_2 = function_2b('Seminars', 'Borrel')['C']
 
-var_3 = str(function_2b(...)) + function_2c(...)
+var_3 = str(function_2c(1, 1000, 10, 1)['multiply']) + function_2b('CLS', '')['L']
 
 if var_1 == 950:
     print("Good job!")
